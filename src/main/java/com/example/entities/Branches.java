33fb95703f2252d0286ce91be0c946bb309e8e5d
Praneeth Marem <Praneeth.Marem@BRILLIO.COM>
2024-06-03 09:34:32 +0530

    initial commit

---
+++ src/main/java/com/example/entities/Branches.java
@@ -0,0 +1,41 @@
+package com.example.entities;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Branches {
+	
+	private List<Branch> branchList;
+
+	public Branches() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Branches(List<Branch> branchList) {
+		super();
+		this.branchList = branchList;
+	}
+
+	@Override
+	public String toString() {
+		return "Branches [branchList=" + branchList + "]";
+	}
+
+	public List<Branch> getBranchList() {
+		return branchList;
+	}
+
+	public void setBranchList(List<Branch> branchList) {
+		this.branchList = branchList;
+	}
+
+}
