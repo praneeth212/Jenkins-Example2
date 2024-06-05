60479eea337800ce5d125c21887684aadb74b514
praneeth212 <147459439+praneeth212@users.noreply.github.com>
2024-06-05 10:16:05 +0530

    Update Employees.java

--- src/main/java/com/example/entities/Employees.java
+++ src/main/java/com/example/entities/Employees.java
@@ -6,10 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-@Data
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 public class Employees {
 	
 	private int bid;
