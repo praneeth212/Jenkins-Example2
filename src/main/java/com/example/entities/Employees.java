ab5e378d9a740363f7446a6a3e0692728afca403
praneeth212 <147459439+praneeth212@users.noreply.github.com>
2024-06-05 10:17:16 +0530

    Update Employees.java

--- src/main/java/com/example/entities/Employees.java
+++ src/main/java/com/example/entities/Employees.java
@@ -11,15 +11,15 @@ public class Employees {
 	private int bid;
 	private List<Employee> empList;
 	
-	// public Employees() {
-	// 	super();
+	 public Employees() {
+	 	super();
 	// 	// TODO Auto-generated constructor stub
-	// }
-	// public Employees(int bid, List<Employee> empList) {
-	// 	super();
-	// 	this.bid = bid;
-	// 	this.empList = empList;
-	// }
+	 }
+	 public Employees(int bid, List<Employee> empList) {
+	 	super();
+	 	this.bid = bid;
+	 	this.empList = empList;
+	 }
 	@Override
 	public String toString() {
 		return "Employees [bid=" + bid + ", empList=" + empList + "]";
